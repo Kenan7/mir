5763d1c86e448e8cf363fba6e8e7b05d9e9250b7
Kenan <kenankzmv@gmail.com>
2023-11-16 10:58:53 +0400

    Add BlogArticle model and GenericModelMixin
    This commit adds the BlogArticle model with fields for title, slug,
    content, author, and is_published. It also includes a prepare_slug
    method to generate a unique slug for each article. Additionally, this
    commit adds a GenericModelMixin abstract model with created_at and
    updated_at fields.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from core.models import GenericModelMixin
+
+User = get_user_model()
+
+
+class BlogArticle(GenericModelMixin):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+    
+    def prepare_slug(self):
+        self.slug = self.title.lower().replace(' ', '-')
+
+        title_already_exists = BlogArticle.objects.filter(title=self.title).exists()
+        if title_already_exists:
+            self.slug += f'-{self.id}'
+
+    def save(self, *args, **kwargs):
+        self.prepare_slug()
+        super().save(*args, **kwargs)
