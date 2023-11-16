d39173ff2b83d096d3475235c54948286966f058
Kenan <kenankzmv@gmail.com>
2023-11-16 11:03:53 +0400

    Add ContactRequest model to admin page with read-only fields

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from core.models import ContactRequest
 
-# Register your models here.
+
+@admin.register(ContactRequest)
+class ContactRequestAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'created_at']
+    list_filter = ('name', 'email')
+    readonly_fields = ['name', 'email', 'message', 'created_at']
+    search_fields = ('name', 'email')
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
--- core/models.py
+++ core/models.py
@@ -7,3 +7,12 @@ class GenericModelMixin(models.Model):
 
     class Meta:
         abstract: bool = True
+
+
+class ContactRequest(GenericModelMixin):
+    email = models.EmailField()
+    name = models.CharField(max_length=100)
+    message = models.TextField()
+
+    def __str__(self):
+        return f'Contact Request from {self.name} - {self.message[:20]}'
